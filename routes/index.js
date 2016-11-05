ff0de4825537c10aa18bc20854a19afc2c1fdeb4
Rajat Bajaj <rajatbajaj56@gmail.com>
2016-11-05 12:31:09 +0530

    routes folder modified

--- routes/index.js
+++ routes/index.js
@@ -9,16 +9,23 @@ var auth = require('controllers/login');
 
 router.post('/login', auth.login_user);
 
+
+
 /*
  * Routes that can be accessed only by authenticated & authorized users
 */
-router.get('/api/v1/admin/users',auth.checkIt);
-/*
-router.get('/api/v1/admin/user/:id', user.getOne);
-router.post('/api/v1/admin/user/', user.create);
-router.put('/api/v1/admin/user/:id', user.update);
-router.delete('/api/v1/admin/user/:id', user.delete);
-*/
+router.get('/api/v1/*/*',function(req,res){
+
+	//getting value for first *
+	var module_name = req.params['0'];
+	//getting value for second *
+	var function_name = req.params['1'];
+
+	var module = require('controllers/'+module_name);
+	//calling the function for the corrosponding module
+	module[function_name](req,res);
+});
+
 
 
 module.exports = router;
