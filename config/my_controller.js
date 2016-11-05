75475bb3a4ba3c5943abe9f2d946ab0e23d21737
Rajat Bajaj <rajatbajaj56@gmail.com>
2016-11-05 12:56:34 +0530

    modified my_controller.js

--- config/my_controller.js
+++ config/my_controller.js
@@ -1,18 +1,58 @@
 /*
  * author : Rajat Bajaj
 */
+
+/*
+ * It is a user defined controller that shows the module
+ * according to the user authorisation
+ * if user is student or empoloyee or admin
+ * corrosponding modules are shown
+*/
+
 var Session = require(config/session);
 
+const modules_table = 'modules';
 
 function MyController(params = [],token) {
 	//params is the array defining the auth_ids' that can access the module
 	this.args = params;
-	this.params = new Session(token);
+	this.session = new Session(token);
 }
 
 MyController.prototype = {
 
-	getMenu() : function(){
+	getMenu() : function(callback){
 		console.log(params);
+		var user_id = session.getId();
+		var auths = session.getAuth();
+
+		getAllModules(function(err,result))
+		{
+			if(err)
+			{
+				callback(err,{'err_code':6});
+			}
+			else
+			{
+				//Go to all the modules and get the auths associated with them and show to user;
+			}
+		}
 	},
+
+
 }
+
+function getAllModules(callback){
+	var query = 'SELECT * FROM ' + modules_table + 'WHERE 1';
+	db.query(query,params,function(err,result){
+		if(err)
+		{
+			callback(err,{});
+		}
+		else
+		{
+			callback(err,result);
+		}
+	});
+}
+
